8eeffbe143436bfe3c38c533ed4909d5b625ba5e
Harry Yang <harry.seunghoon.yang@gmail.com>
2017-06-26 17:54:16 +0900

    chapter3 cleared

--- ggplot2 - hadley wickham/chapter3.R
+++ ggplot2 - hadley wickham/chapter3.R
@@ -304,3 +304,180 @@ ggplot(mi_cities, aes(lon, lat)) +
   coord_quickmap()
 
 # 3.7.3 Raster Images
+
+if (file.exists("mi_raster.rds")) {
+  mi_raster <- readRDS("mi_raster.rds")
+} else {
+  bbox <- c(
+    min(mi_counties$lon), min(mi_counties$lat),
+    max(mi_counties$lon), max(mi_counties$lat)
+  )
+  mi_raster <- ggmap::get_openstreetmap(bbox, scale = 8735660)
+  saveRDS(mi_raster, "mi_raster.rds")
+}
+
+ggmap::ggmap(mi_raster)
+
+ggmap::ggmap(mi_raster) +
+  geom_point(aes(size=pop), mi_cities, color="red") +
+  scale_size_area()
+
+df <- as.data.frame(raster::rasterToPoints(x))
+names(df) <- c("lon", "lat", "x")
+
+ggplot(df, aes(lon, lat)) +
+  geom_raster(aes(fill=x))
+
+# 3.7.4 Area Metadata
+mi_census <- midwest %>% 
+  tbl_df() %>% 
+  filter(state =="MI") %>% 
+  mutate(county = tolower(county)) %>% 
+  select(county, area, poptotal, percwhite, percblack)
+
+mi_census
+
+census_counties <- left_join(mi_census, mi_counties, by=c("county"="id"))
+census_counties
+
+ggplot(census_counties, aes(lon, lat, group=county)) +
+  geom_polygon(aes(fill=poptotal)) +
+  coord_quickmap()
+
+ggplot(census_counties, aes(lon, lat, group=county)) +
+  geom_polygon(aes(fill = percwhite)) +
+  coord_quickmap()
+
+# 3.8 Revealing Uncertainty
+
+y <- c(18, 11, 16)
+df <- data.frame(x = 1:3, y=y, se=c(1.2, 0.5, 1.0))
+
+base <- ggplot(df, aes(x, y, ymin=y-se, ymax=y+se))
+base + geom_crossbar()
+base + geom_pointrange()
+base + geom_smooth(stat = "identity")
+base + geom_errorbar()
+base + geom_linerange()
+base + geom_ribbon()
+
+# 3.9 Weighted Data
+
+## Unweighted
+ggplot(midwest, aes(percwhite, percbelowpoverty)) +
+  geom_point()
+
+## Weight by population
+ggplot(midwest, aes(percwhite, percbelowpoverty)) +
+  geom_point(aes(size=poptotal / 1e6)) +
+  scale_size_area("Population\n(millions)", breaks = c(0.5, 1, 2, 4))
+
+## Unweighted
+ggplot(midwest, aes(percwhite, percbelowpoverty)) +
+  geom_point() +
+  geom_smooth(method = lm, size=1)
+
+pc <- ggplot(midwest, aes(percwhite, percbelowpoverty))
+pc + geom_point(aes(size = poptotal / 1e6)) +
+  geom_smooth(aes(weight = poptotal), method = lm, size = 1) +
+  scale_size_area(guide = "none")
+
+ggplot(midwest, aes(percbelowpoverty)) +
+  geom_histogram(binwidth = 1) +
+  ylab("Counties")
+
+ggplot(midwest, aes(percbelowpoverty)) +
+  geom_histogram(aes(weight = poptotal), binwidth=1) +
+  ylab("Population (1000s)")
+
+# 3.10 Diamonds Data
+
+diamonds
+
+# 3.11 Displaying Distributions
+
+ggplot(diamonds, aes(depth)) +
+  geom_histogram()
+ggplot(diamonds, aes(depth)) +
+  geom_histogram(binwidth=0.1) +
+  xlim(55, 70)
+
+ggplot(diamonds, aes(depth)) +
+  geom_freqpoly(aes(color=cut), binwidth=0.1, na.rm=T) +
+  xlim(58, 68) +
+  theme(legend.position = "none")
+ggplot(diamonds, aes(depth)) +
+  geom_histogram(aes(fill=cut), binwidth=0.1, position="fill", na.rm=T) +
+  xlim(58, 68) +
+  theme(legend.position = "none")
+
+ggplot(diamonds, aes(depth)) +
+  geom_density(na.rm = T) +
+  xlim(58, 68) +
+  theme(legend.position = "none")
+ggplot(diamonds, aes(depth, fill=cut, color=cut)) +
+  geom_density(alpha = 0.2, na.rm = T) +
+  xlim(58, 68) +
+  theme(legend.position = "none")
+
+ggplot(diamonds, aes(clarity, depth)) +
+  geom_boxplot()
+
+ggplot(diamonds, aes(carat, depth)) +
+  geom_boxplot(aes(group=cut_width(carat, 0.1))) +
+  xlim(NA, 2.05)
+
+ggplot(diamonds, aes(clarity, depth)) +
+  geom_violin()
+ggplot(diamonds, aes(carat, depth)) +
+  geom_violin(aes(group = cut_width(carat, 0.1))) +
+  xlim(NA, 2.05)
+
+# 3.11.1 Exercises
+ggplot(diamonds, aes(price)) +
+  geom_histogram()
+
+ggplot(diamonds, aes(clarity, price)) +
+  geom_boxplot()
+
+ggplot(diamonds, aes(depth)) +
+  geom_freqpoly(aes(group=cut_width(carat, 0.1)))
+
+# 3.12 Dealing with Overplotting
+
+df <- data.frame(x = rnorm(2000), y=rnorm(2000))
+norm <- ggplot(df, aes(x, y)) + xlab(NULL) + ylab(NULL)
+norm + geom_point()
+norm + geom_point(shape = 1)
+norm + geom_point(shape = ".")
+
+norm + geom_point(alpha = 1/3)
+norm + geom_point(alpha = 1/5)
+norm + geom_point(alpha = 1/10)
+
+norm + geom_bin2d()
+norm + geom_bin2d(bins = 10)
+
+norm + geom_hex()
+norm + geom_hex(bins=10)
+
+# 3.13 Statistical Summaries
+
+ggplot(diamonds, aes(color)) +
+  geom_bar()
+
+ggplot(diamonds, aes(color, price)) +
+  geom_bar(stat = "summary_bin", fun.y = mean)
+
+ggplot(diamonds, aes(table, depth)) +
+  geom_bin2d(binwidth = 1, na.rm=T) +
+  xlim(50, 70) +
+  ylim(50, 70)
+
+ggplot(diamonds, aes(table, depth, z=price)) +
+  geom_raster(binwidth = 1, stat="summary_2d", fun=mean, na.rm=T) +
+  xlim(50, 70) +
+  ylim(50, 70)
+
+# 3.14 Add-on Packages
+

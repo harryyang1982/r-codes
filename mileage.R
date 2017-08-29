4ec11f35a82b38571d9acb326cb5eaa2dbfb77e4
harryyang1982 <harry.seunghoon.yang@gmail.com>
2017-08-29 20:35:02 +0900

    dd

--- mileage.R
+++ mileage.R
@@ -1,16 +1,16 @@
 library(readxl)
 library(tidyverse)
 
-mileage <- read_excel("mileage2017.xlsx", skip=2)
-mileage <- mileage[-c(131:138),]
+mileage <- read_excel("mileage2017.xlsx", skip=2, sheet=2)
+mileage <- mileage[-c(151:158),]
 mileage <- mileage[,-1]
 
 
 mileage %>% 
   filter(마일리지합계 != 0 & 마일리지합계 > 50) %>% 
-  summarise(SUM=sum(마일리지합계), AVERAGE=mean(마일리지합계), N=n())
+  summarise(SUM=sum(마일리지합계), AVERAGE=mean(마일리지합계), SD=sd(마일리지합계), MAX=max(마일리지합계), MIN=min(마일리지합계), N=n())
 
-mileage %>% 
-  summarise(SUM=sum(마일리지합계), AVERAGE=mean(마일리지합계), N=n())
+statm <- mileage %>% 
+  summarise(SUM=sum(마일리지합계), AVERAGE=mean(마일리지합계), SD=sd(마일리지합계), MAX=max(마일리지합계), MIN=min(마일리지합계), N=n())
 
 marinemap <- read_excel("marinemapdb.xlsx")
